344bce99ee9e122306432cb8b9777e571546ab6b
Caroline Orsi <caroline.orsi@gmail.com>
2015-02-06 13:59:38 -0800

    Initial commit of tree files.

---
+++ Trees/tree_tests.py
@@ -0,0 +1,15 @@
+import unittest
+from linked_list import *
+
+
+class TestTrees(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_(self):
+        self.assertEqual(5, 5)
+
+
+if __name__ == '__main__':
+    unittest.main()
