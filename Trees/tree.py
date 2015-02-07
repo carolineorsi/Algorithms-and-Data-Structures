a9d82a3ac5bd2c2389700e8a70f9f36f7811011c
Caroline Orsi <caroline.orsi@gmail.com>
2015-02-06 16:42:26 -0800

    Added depth first traversal function.)

--- Trees/tree.py
+++ Trees/tree.py
@@ -5,8 +5,23 @@ class BinaryTreeNode():
         self.right = None
         self.left = None
 
+
     def insertRight(self, value):
         self.right = BinaryTreeNode(value)
 
+
     def insertLeft(self, value):
         self.left = BinaryTreeNode(value)
+
+
+
+def depth_first_traversal(root):
+    traversal_list = [root.value]
+
+    if root.left:
+        traversal_list.extend(depth_first_traversal(root.left))
+    
+    if root.right:
+        traversal_list.extend(depth_first_traversal(root.right))
+
+    return traversal_list
--- Trees/tree_tests.py
+++ Trees/tree_tests.py
@@ -9,11 +9,14 @@ class TestTrees(unittest.TestCase):
         self.tree.insertLeft(2)
         self.tree.insertRight(3)
 
-    def test_right(self):
+    def test_tree(self):
+        self.assertEqual(self.tree.value, 1)
         self.assertEqual(self.tree.right.value, 3)
-
-    def test_left(self):
         self.assertEqual(self.tree.left.value, 2)
+
+    def test_depth_first_traversal(self):
+        self.assertEqual(depth_first_traversal(self.tree), [1, 2, 3])
+
 
 
 if __name__ == '__main__':
