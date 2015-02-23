2c70edde1e33d3a7a09adf5671ee41bb64b2ea51
Caroline Orsi <caroline.orsi@gmail.com>
2015-02-23 11:43:27 -0800

    Added function and tests to return product of nums in array.

--- Recursion/recursion.py
+++ Recursion/recursion.py
@@ -129,8 +129,10 @@ def fibonacci(n):
     >>> fibonacci(9)
     34
     """
+    if n <= 0:
+        return 0
 
-    if n <= 2:
+    if n == 1:
         return 1
 
     return fibonacci(n-1) + fibonacci(n-2)
